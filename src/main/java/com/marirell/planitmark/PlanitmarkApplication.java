d54658ae4034f7223caca70005c77782c2de7340
M Coindreau <mariannacoindreau@gmail.com>
2018-01-29 13:35:45 -0600

    Initial commit

---
+++ src/main/java/com/marirell/planitmark/PlanitmarkApplication.java
@@ -0,0 +1,12 @@
+package com.marirell.planitmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanitmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanitmarkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/marirell/planitmark/PlanitmarkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marirell.planitmark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlanitmarkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
